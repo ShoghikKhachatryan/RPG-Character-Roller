9b6efe44eead2b61a6358cc4c824bc16881facd4
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-05 19:05:55 +0200

    Add .gitignore file. Refactor code

--- Creator.java
+++ Creator.java
@@ -1,9 +1,10 @@
 import java.util.Random;
 
 public class Creator {
+
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Write your name");
+            System.out.println("Write a name");
             return;
         }
 
@@ -13,31 +14,31 @@ public class Creator {
         Random random = new Random();
         int n = random.nextInt(3);
 
-        // 2 included 2 not 13 for stats
-        int  statsStrength = random.nextInt(11) + 2;
-        int  statsMagic = random.nextInt(11) + 2;
-        int  statsHealth = random.nextInt(11) + 2;
+        // Random [2, 13)
+        int  strengthState = random.nextInt(11) + 2;
+        int  magicState = random.nextInt(11) + 2;
+        int  healthState = random.nextInt(11) + 2;
 
         String be;
         switch (n) {
             case 0:
                 be = "Warrior";
-                statsStrength *= 3;
+                strengthState *= 3;
                 break;
             case 1:
                 be = "Wizard";
-                statsMagic *= 3;
+                magicState *= 3;
                 break;
             default:
                 be = "Potato";
-                statsHealth *= 3;
-                break;                 // This is not necessary, but it is good style. For adding other cases
+                healthState *= 3;
+                break; // This is not necessary, but it is good style. For adding other cases
         }
 
         System.out.println(args[0] + " is.... a " + be);
-        System.out.println("\tStrength: " + statsStrength);
-        System.out.println("\tMagic: " + statsMagic);
-        System.out.println("\tHealth: " + statsHealth);
+        System.out.println("\tStrength: " + strengthState);
+        System.out.println("\tMagic: " + magicState);
+        System.out.println("\tHealth: " + healthState);
         System.out.println("Happy adventuring!");
         System.out.println("-----------------------------------------");
     }
